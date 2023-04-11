c75da529f09802ad2dc2e60c606cf9fab8ae3e43
searce-thejaswini <107244346+searce-thejaswini@users.noreply.github.com>
2023-04-11 15:46:01 +0530

    Create provider.tf

---
+++ standard-private/gke-cluster/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">=3.61.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">=3.61.0"
+    }
+  }
+}
