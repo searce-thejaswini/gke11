737710e6cd19ca13561564ac0fb46dbe9db83aaa
searce-thejaswini <107244346+searce-thejaswini@users.noreply.github.com>
2023-04-11 15:50:29 +0530

    Create outputs.tf

---
+++ standard-Public/gke-cluster/outputs.tf
@@ -0,0 +1,23 @@
+output "cluster_name" {
+    description = "The name of cluster"
+    value = google_container_cluster.searce_cluster.name
+}
+
+output "project_id" {
+    description = "The projec name of cluster"
+    value = google_container_cluster.searce_cluster.project
+}
+
+output "node_version" {
+    description = "The gke version of cluster"
+    value = google_container_cluster.searce_cluster.node_version
+}
+
+
+output "cluster_region" {
+   value = google_container_cluster.searce_cluster.location
+}
+
+output "cluster_region_zones" {
+   value = google_container_cluster.searce_cluster.node_locations
+}
